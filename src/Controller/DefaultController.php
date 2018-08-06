3a2cf35766867ca74568a844fa503d3f289c22ce
cubiascaceres <cubiascaceres@gmail.com>
2018-08-05 18:15:20 -0600

    Adding Home & Admin controller. configuring secutiry.YAML

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,34 +2,8 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends Controller
-{
-    /**
-     * @Route("/default", name="default")
-     */
-    public function index()
-    {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
-    }
-    
-    /**
-     * @Route("/admin")
-     */
-    public function admin()
-    {
-        return new Response('<html><body>Admin page!</body></html>');
-    }
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function home()
-    {
-        return new Response('<html><body>Home page!</body></html>');
-    }  
+class DefaultController extends Controller {
+
 }
---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/home", name="homepage")
+     */
+    public function home()
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
