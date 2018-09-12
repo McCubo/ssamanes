050a15a8a23e6ad47a0adf3efa4b93a2b671fd37
McCubo <cubiascaceres@gmail.com>
2018-09-12 14:02:14 -0600

    Binding parameter, sending authorization_checker instead of service container

--- src/EventListener/RequestListener.php
+++ src/EventListener/RequestListener.php
@@ -7,17 +7,16 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class RequestListener {
     private $router;
-    private $container;
+    private $authChecker;
 
-    public function __construct ($router, $container) {
+    public function __construct ($router, $authChecker) {
         $this->router = $router;
-        $this->container = $container;
+        $this->authChecker = $authChecker;
     }
 
     public function onKernelRequest (GetResponseEvent $event) {
         $request = $event->getRequest()->getRequestUri();
-        if ($request == '/' && $event->isMasterRequest() && $this->container->get(
-                'security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+        if ($request == '/' && $event->isMasterRequest() && $this->authChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
             $url = $this->router->generate('homepage');
             $event->setResponse(new RedirectResponse($url));
         }
